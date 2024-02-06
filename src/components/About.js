b96b90196232b0506140fb8a437d3d9e8393e0be
AlucardM13 <mr6174@nyu.edu>
2024-02-07 02:48:24 +0400

    Make "Step 1", "Step 2" etc. bold for styling

--- src/components/About.js
+++ src/components/About.js
@@ -32,16 +32,16 @@ function About() {
     <div className="content" >
       <h1 className="aboutapp" data-aos="fade-down">Introducing the <span style={{color:"#1F9C89"}}>Pet Passport</span> on the PetRepublic app!</h1>
       <div className="textbox" data-aos="fade-left">
-      <p>Step 1: Choose "Create Passport" on the app.</p>
+      <p><strong>Step 1 : </strong> Choose "Create Passport" on the app.</p>
         </div>
         <div className="textbox" data-aos="fade-right">
-          <p>Step 2: Select your payment plan.</p>
+          <p><strong>Step 2 : </strong> Select your payment plan.</p>
         </div>
         <div className="textbox" data-aos="fade-left">
-          <p>Step 3: Fill out the payment details.</p>
+          <p><strong>Step 3 : </strong> Fill out the payment details.</p>
         </div>
         <div className="textbox" data-aos="fade-right">
-          <p>Step 4: Fill out your fur baby's details!</p>
+          <p><strong>Step 4 : </strong> Fill out your fur baby's details!</p>
         </div>
       {/* <img src={van} className="doglogo" alt="Dog Logo" style={{ transform: `translate3d(${scrollPosition-scrollPosition}px, ${-scrollPosition}px, 0)` }}/> */}
     </div> 
