bb971c07744907fa5ef9d2b6806a5c5399aa77e3
AlucardM13 <mr6174@nyu.edu>
2024-02-06 16:44:36 +0400

    Added more SVGs and tested footer designs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,17 +14,17 @@ const Footer = () => {
           <TwitterShareButton url="https://yourwebsite.com">
             <TwitterIcon size={32} round={true} />
           </TwitterShareButton>
-          <LinkedinShareButton url="https://yourwebsite.com">
+          <LinkedinShareButton url="https://www.linkedin.com/company/petsrepublic/">
             <LinkedinIcon size={32} round={true} />
           </LinkedinShareButton>
-          <a href="https://www.instagram.com/yourusername/" className="InstaIcon" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/petrepublic.ae?igsh=cXI3MTd0a3B0OXZs" className="InstaIcon" target="_blank" rel="noopener noreferrer">
             <FaInstagram size={32} color='black'/>
           </a>
         </div>
         <div className="footer-links">
-          <a href="/about">About Us</a>
-          <a href="/contact">Contact</a>
-          <a href="/privacy">Privacy Policy</a>
+          <p>About Us</p>
+          <p>Contact</p>
+          <p>Privacy Policy</p>
         </div>
         <div className="footer-company-name">
           @ 2024 Pet Republic
