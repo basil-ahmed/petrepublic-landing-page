1513cf61d0d0c3e3619fd49a65bfa993bb42e349
AlucardM13 <mr6174@nyu.edu>
2024-02-15 14:11:04 +0400

    Linked Dev team T&Cs and Privacy Policy. Also, created my own custom webpages for those and have kept them in case they are needed.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';  
 import Home from './components/Home';      
 import About from './components/About';    
 import Services from './components/Services';  
 import PageFooter from './components/Footer';  
 import MailingList from './components/MailingList';
+import PrivacyPolicy from './components/PrivacyPolicy'; // Ensure this component exists
+import TermsAndConditions from './components/TermsAndConditions'; // Ensure this component exists
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div id="home">
-        <Home />
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <div id="home"><Home /></div>
+              <div id="about"><About /></div>
+              <div id="services"><Services /></div>
+              <div id="mailing-list"><MailingList /></div>
+            </>
+          } exact />
+          <Route path="/home" element={<Home />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        </Routes>
+        <PageFooter />
       </div>
-      <div id="about">
-        <About />
-      </div>
-      <div id="services">
-        <Services />
-      </div>
-      <div id="mailing-list">
-        <MailingList />
-      </div>
-      <PageFooter />
-    </div>
+    </Router>
   );
 }
 
