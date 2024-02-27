d80ad3223208d6fee3abc7983cfeb5bd2d9495e4
basil-ahmed <bq2024@nyu.edu>
2024-02-27 18:17:23 +0400

    fixed Grow With Us

--- src/components/OnboardingForm.js
+++ src/components/OnboardingForm.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import FormfacadeEmbed from "@formfacade/embed-react";
+import './OnboardingForm.css';
 
 function OnboardingForm() {
 
     return (
+        <div className='onboarding-form'>
+        <div className="onboarding-header">
+          <h2>Pet Republic</h2>
+          <h1>Grow your business with PetRepublic! </h1>
+          <p className='onboarding-desc'>Join forces with us to connect with a wider audience, increase your earnings, and thrive online – your journey to success starts right here.</p>
+        </div>
+
         <FormfacadeEmbed
 
 formFacadeURL="https://formfacade.com/include/105076702415125403461/form/1FAIpQLSfffLLNvbsp_6Og9sG3DTvwB8YWtNy1gVUiAQV6JgOjpOYJ0w/classic.js/?div=ff-compose"
@@ -11,6 +19,7 @@ formFacadeURL="https://formfacade.com/include/105076702415125403461/form/1FAIpQL
 onSubmitForm={() => console.log('Form submitted')}
 
 />
+</div>
     );
 }
 
